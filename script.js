4b9dcf4880f412b0f9d4e4f161d546148b424113
satork <sebastian.kedzier@gmail.com>
2024-02-12 19:09:28 +0100

    JS added - removing grid in button click function, mouseover box coloring listener

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 let gridSize = document.createElement("input");
 let gridSizeButton = document.createElement("button");
 
-
 // STYLIZE BUTTON AND INPUT
 gridSizeButton.innerText = "Make";
 gridSizeButton.style.margin = "0 0 0 10px";
@@ -20,30 +19,41 @@ let container = document.querySelector(".container");
 body.insertBefore(gridSize, container);
 body.insertBefore(gridSizeButton, container);
 
-const gridFinal = () => {
-    // WIDTH,HEIGHT VALUE of BOX
-    let divWidth = 600 / gridSize.value + "px";
-    let divHeight = 600 / gridSize.value + "px";
+// CREATING A DIV GRID BY BUTTON
+gridSizeButton.addEventListener("click", () => {
+  if (document.querySelector(".box")) {
+    document.querySelectorAll(".box").forEach((box) => box.remove());
+  }
 
-    // SAFETY FOR TO LOW OR TO HIGH NUMBER INSERTION
-    if (gridSize.value > 100) {
-        alert("to high number")
-    } else if (gridSize.value < 1) {
-        alert("to low number")
-    } else {
+  // WIDTH,HEIGHT VALUE of BOX
+  let divWidth = 600 / gridSize.value + "px";
+  let divHeight = 600 / gridSize.value + "px";
+
+  // SAFETY FOR TO LOW OR TO HIGH NUMBER INSERTION
+  if (gridSize.value > 100) {
+    alert("to high number");
+  } else if (gridSize.value < 1) {
+    alert("to low number");
+  } else {
     for (let i = 0; i < gridSize.value * gridSize.value; i++) {
-        let div = document.createElement("div");
-        container.appendChild(div);
+      let div = document.createElement("div");
+      div.classList = "box";
+      container.appendChild(div);
 
-        div.style.width = divWidth;
-        div.style.height = divHeight;
+      div.style.width = divWidth;
+      div.style.height = divHeight;
     }
-    }
+  }
 
-};
+  let singleDiv = document.querySelectorAll(".box");
+
+  // COLOR AN DIV
+
+  singleDiv.forEach((el) => { 
+    el.addEventListener("mouseover", () => {
+        el.style.backgroundColor = "red"
+  });}) 
 
 
 
-
-// CREATING A DIV-CONTAINER
-gridSizeButton.addEventListener("click", gridFinal);
+});
